8887c268b89536f0d9b0713b15eda23342209415
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-06-08 15:28:19 +0530

    added header component

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
 import HomePage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
+import Header from './components/header/header';
+import './App.css';
 
 function App(props) {
 	return (
 		<div>
+			<Header />
 			{/* match everything woth / or /* */}
 			<Switch>
 				<Route exact path='/' component={HomePage} />
