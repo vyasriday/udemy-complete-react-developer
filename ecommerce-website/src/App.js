d140bc3859d81105627b852c8764cf7d2afd17cd
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-06-08 22:20:52 +0530

    added a resuable formInput component and used it in sign in component

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
 import Header from './components/header/header';
+import SignInAndSingOutPage from './pages/sign-in-sign-out/SignInAndSignOut';
 import './App.css';
 
 function App(props) {
@@ -13,6 +14,7 @@ function App(props) {
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route path='/shop' component={ShopPage} />
+				<Route path='/signin' component={SignInAndSingOutPage} />
 			</Switch>
 		</div>
 	);
